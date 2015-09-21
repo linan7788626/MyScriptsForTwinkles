2024ccf55643b813d26a9a4724030bddfac0fcef
Nan Li <linan7788626@gmail.com>
2015-09-21 13:51:56 -0500

    examples for GalSim

---
+++ GalSim_scripts/test_roll.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#for i in $( ls ./finalcats ); do
+
+#python my_mmbr.py -i ./$1/$2 -o ./$1/$2 -f g  
+#python my_mmbr.py -i ./$1/$2 -o ./$1/$2 -f r 
+#python my_mmbr.py -i ./$1/$2 -o ./$1/$2 -f i 
+#python my_mmbr.py -i ./$1/$2 -o ./$1/$2 -f z
+
+#python my_mmbr.py -i ./finalcats/$1_clustgalcat_$2ran_sept.cat -o ../final_images/final_$1_mem_$2ran -f r &
+#python my_mmbr.py -i ./finalcats/$1_clustgalcat_$2ran_sept.cat -o ../final_images/final_$1_mem_$2ran -f i &
+#python my_mmbr.py -i ./finalcats/$1_clustgalcat_$2ran_sept.cat -o ../final_images/final_$1_mem_$2ran -f z &
+
+INDEX=6
+
+python ./go_back.py -i ./clust_sept2/${INDEX}_clustgalcat_0.364ran_sept2.cat -o ./clust_sept2/${INDEX}_clustgalcat_0.364ran_sept2.cat -f g
+python ./go_back.py -i ./clust_sept2/${INDEX}_clustgalcat_0.364ran_sept2.cat -o ./clust_sept2/${INDEX}_clustgalcat_0.364ran_sept2.cat -f r
+python ./go_back.py -i ./clust_sept2/${INDEX}_clustgalcat_0.364ran_sept2.cat -o ./clust_sept2/${INDEX}_clustgalcat_0.364ran_sept2.cat -f i
+python ./go_back.py -i ./clust_sept2/${INDEX}_clustgalcat_0.364ran_sept2.cat -o ./clust_sept2/${INDEX}_clustgalcat_0.364ran_sept2.cat -f z
+         go_back                                                                                      
+python ./go_back.py -i ./clust_sept2/${INDEX}_clustgalcat_0.539ran_sept2.cat -o ./clust_sept2/${INDEX}_clustgalcat_0.539ran_sept2.cat -f g
+python ./go_back.py -i ./clust_sept2/${INDEX}_clustgalcat_0.539ran_sept2.cat -o ./clust_sept2/${INDEX}_clustgalcat_0.539ran_sept2.cat -f r
+python ./go_back.py -i ./clust_sept2/${INDEX}_clustgalcat_0.539ran_sept2.cat -o ./clust_sept2/${INDEX}_clustgalcat_0.539ran_sept2.cat -f i
+python ./go_back.py -i ./clust_sept2/${INDEX}_clustgalcat_0.539ran_sept2.cat -o ./clust_sept2/${INDEX}_clustgalcat_0.539ran_sept2.cat -f z
+         go_back                                                                                       
+python ./go_back.py -i ./clust_sept2/${INDEX}_clustgalcat_0.736ran_sept2.cat -o ./clust_sept2/6${INDEX}_clustgalcat_0.736ran_sept2.cat -f g
+python ./go_back.py -i ./clust_sept2/${INDEX}_clustgalcat_0.736ran_sept2.cat -o ./clust_sept2/6${INDEX}_clustgalcat_0.736ran_sept2.cat -f r
+python ./go_back.py -i ./clust_sept2/${INDEX}_clustgalcat_0.736ran_sept2.cat -o ./clust_sept2/6${INDEX}_clustgalcat_0.736ran_sept2.cat -f i
+python ./go_back.py -i ./clust_sept2/${INDEX}_clustgalcat_0.736ran_sept2.cat -o ./clust_sept2/6${INDEX}_clustgalcat_0.736ran_sept2.cat -f z
