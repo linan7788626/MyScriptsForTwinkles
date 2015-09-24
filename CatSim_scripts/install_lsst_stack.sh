093a59db24cafd998ba3ab50390a024559c867b2
LSST Stack <linan7788626@gmail.com>
2015-09-24 16:33:52 -0500

    update installation script for lsst_stack and lsst_sims

--- CatSim_scripts/install_lsst_stack.sh
+++ CatSim_scripts/install_lsst_stack.sh
@@ -1,3 +1,6 @@
+########################################################################
+#Install using conda
+########################################################################
 conda config --add channels http://eupsforge.net/conda/dev
 conda install lsst-sims
 
@@ -5,22 +8,16 @@ source /Users/<username>/anaconda/bin/eups-setups.sh
 setup lsst_sims
 conda update lsst-sims
 
-
+########################################################################
+#Install from source
+########################################################################
 unset LSST_HOME EUPS_PATH LSST_DEVEL EUPS_PKGROOT REPOSITORY_PATH
 
 mkdir -p /the/LSST/installation/root && cd /the/LSST/installation/root
-
 curl -OL https://sw.lsstcorp.org/eupspkg/newinstall.sh
 bash newinstall.sh
-
 source $INSTALL_DIR/loadLSST.bash # for bash users
-
 eups distrib install -t v10_1 lsst_apps
-
-
-mkdir -p ~/lsst
-cd ~/lsst
-curl -O https://sw.lsstcorp.org/eupspkg/newinstall.sh
-bash newinstall.sh
-source ~/lsst/loadLSST.csh
 eups distrib install lsst_sims -t sims
+setup lsst_sims
+
