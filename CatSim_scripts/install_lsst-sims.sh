47e1d7869ac708abb1ae4733b3c7db6e932d03e3
LSST Stack <linan7788626@gmail.com>
2015-09-24 19:09:42 -0500

    update usage of catsim.

--- CatSim_scripts/install_lsst-sims.sh
+++
@@ -1,6 +0,0 @@
-conda config --add channels http://eupsforge.net/conda/dev
-conda install lsst-sims
-
-source /Users/<username>/anaconda/bin/eups-setups.sh
-setup lsst_sims
-conda update lsst-sims
--- CatSim_scripts/install_lsst_stack.sh
+++ CatSim_scripts/install_lsst_stack.sh
@@ -1,5 +1,5 @@
 ########################################################################
-#Install using conda
+# Install using conda
 ########################################################################
 conda config --add channels http://eupsforge.net/conda/dev
 conda install lsst-sims
@@ -9,15 +9,49 @@ setup lsst_sims
 conda update lsst-sims
 
 ########################################################################
-#Install from source
+# Install from source
 ########################################################################
-unset LSST_HOME EUPS_PATH LSST_DEVEL EUPS_PKGROOT REPOSITORY_PATH
+-- Create a new user.
+-- unset LSST_HOME EUPS_PATH LSST_DEVEL EUPS_PKGROOT REPOSITORY_PATH
+-- mkdir -p /the/LSST/installation/root && cd /the/LSST/installation/root
+-- curl -OL https://sw.lsstcorp.org/eupspkg/newinstall.sh
+-- bash newinstall.sh
+-- source $INSTALL_DIR/loadLSST.bash # for bash users
+-- eups distrib install -t v10_1 lsst_apps
+-- eups distrib install lsst_sims -t sims
+-- setup lsst_sims
+########################################################################
+# Running examples of CatSim_GalSim interface
+########################################################################
 
-mkdir -p /the/LSST/installation/root && cd /the/LSST/installation/root
-curl -OL https://sw.lsstcorp.org/eupspkg/newinstall.sh
-bash newinstall.sh
-source $INSTALL_DIR/loadLSST.bash # for bash users
-eups distrib install -t v10_1 lsst_apps
-eups distrib install lsst_sims -t sims
-setup lsst_sims
+# building ssh Tunnel
+ssh -L 51433:fatboy-private.phys.washington.edu:1433 simsuser@gateway.astro.washington.edu
 
+# editing config/db.py
+vi $SIMS_CATUTILS_DIR/config/db.py
+#--------------------------------------------------
+root.driver='mssql+pymssql'
+root.host='localhost'
+root.port='51433'
+root.database='LSST'
+#--------------------------------------------------
+
+# Edit username and password
+vi .lsst/db-auth.paf 
+#--------------------------------------------------
+database: {  
+    authInfo: {
+        host: localhost
+        port: 51433
+        user: LSST-2
+        password: L$$TUser
+    }
+}
+#--------------------------------------------------
+# Make it safe
+cd $HOME
+chmod 700 .lsst
+chmod 600 .lsst/db-auth.paf
+
+# Refs
+https://confluence.lsstcorp.org/display/SIM/Accessing+the+UW+CATSIM+Database
