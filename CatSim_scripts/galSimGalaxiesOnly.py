f578396cdcd31e96e3dc193a94fb0fdb4dd136cf
LSST Stack <linan7788626@gmail.com>
2015-10-05 17:33:44 -0500

    produce a set of slices at different redshift successfully.

--- CatSim_scripts/galSimGalaxiesOnly.py
+++ CatSim_scripts/galSimGalaxiesOnly.py
@@ -10,7 +10,6 @@ from lsst.sims.utils import ObservationMetaData
 from lsst.sims.catUtils.baseCatalogModels import StarObj, GalaxyBulgeObj, GalaxyDiskObj, GalaxyAgnObj, \
                                                  OpSim3_61DBObject
 from lsst.sims.GalSimInterface import SNRdocumentPSF, GalSimStars, GalSimGalaxies, GalSimAgn
-from myana import *
 
 #if you want to use the actual LSST camera
 #from lsst.obs.lsstSim import LsstSimMapper
@@ -56,9 +55,16 @@ obsMD = OpSim3_61DBObject()
 obs_metadata = obsMD.getObservationMetaData(88625744, 0.05, makeCircBounds = True)
 
 catName = './outputs_cats/galSim_galaxies_example.txt'
+import sys
+zdn = sys.argv[1]
+zup = sys.argv[2]
+
+#cstrn = "redshift>0.1 and redshift<0.5"
+cstrn = "redshift>"+str(zdn)+" and redshift<="+str(zup)
+print cstrn
 
 bulges = CatalogDBObject.from_objid('galaxyBulge')
-bulge_galSim = testGalSimGalaxies(bulges, obs_metadata=obs_metadata, column_outputs=['redshift'])
+bulge_galSim = testGalSimGalaxies(bulges, obs_metadata=obs_metadata, column_outputs=['redshift'],constraint=cstrn)
 bulge_galSim.noise_and_background = None
 bulge_galSim.PSF = None
 bulge_galSim.write_catalog(catName, write_header=True, write_mode='a')
@@ -66,7 +72,7 @@ bulge_galSim.write_catalog(catName, write_header=True, write_mode='a')
 print 'done with bulges'
 
 disks = CatalogDBObject.from_objid('galaxyDisk')
-disk_galSim = testGalSimGalaxies(disks, obs_metadata=obs_metadata, column_outputs=['redshift'])
+disk_galSim = testGalSimGalaxies(disks, obs_metadata=obs_metadata, column_outputs=['redshift'],constraint=cstrn)
 disk_galSim.copyGalSimInterpreter(bulge_galSim)
 disk_galSim.noise_and_background = None
 disk_galSim.PSF = None
@@ -75,12 +81,12 @@ disk_galSim.write_catalog(catName, write_header=True, write_mode='a')
 print 'done with disks'
 
 agn = CatalogDBObject.from_objid('galaxyAgn')
-agn_galSim = testGalSimAgn(agn, obs_metadata=obs_metadata, column_outputs=['redshift'])
+agn_galSim = testGalSimAgn(agn, obs_metadata=obs_metadata, column_outputs=['redshift'],constraint=cstrn)
 agn_galSim.copyGalSimInterpreter(disk_galSim)
 agn_galSim.noise_and_background = None
 agn_galSim.PSF = None
 agn_galSim.write_catalog(catName, write_header=True, write_mode='a')
 
-agn_galSim.write_images(nameRoot='./outputs_fits/galaxy')
+agn_galSim.write_images(nameRoot='./outputs_fits/galaxy_'+str(zdn)+'_'+str(zup))
 
 print 'done with galaxyAgn'
