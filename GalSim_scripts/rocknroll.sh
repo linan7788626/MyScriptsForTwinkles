0245a745f547bec477a95ea31251da41b6cd8e5b
Nan Li <linan7788626@gmail.com>
2015-10-20 16:55:32 -0500

    update PhoSim scripts.

--- GalSim_scripts/rocknroll.sh
+++ GalSim_scripts/rocknroll.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #for i in $( ls ./finalcats ); do
 
-#python my_mmbr.py -i ./$1/$2 -o ./$1/$2 -f g  
-#python my_mmbr.py -i ./$1/$2 -o ./$1/$2 -f r 
-#python my_mmbr.py -i ./$1/$2 -o ./$1/$2 -f i 
-#python my_mmbr.py -i ./$1/$2 -o ./$1/$2 -f z
+#python ./galsim_no_pixel_run.py -i ./$1/$2 -o ./$1/$2 -f g  
+#python ./galsim_no_pixel_run.py -i ./$1/$2 -o ./$1/$2 -f r 
+#python ./galsim_no_pixel_run.py -i ./$1/$2 -o ./$1/$2 -f i 
+#python ./galsim_no_pixel_run.py -i ./$1/$2 -o ./$1/$2 -f z
 
-python ./galsim_no_pixel_run.py -i ./clust_sept5/mod0_clustgalcat_0.364ran_sept5.cat -o test -f g
+python ./galsim_no_pixel_run.py -i ./example_catalog_for_galsim.cat -o test -f g
+python ./galsim_no_pixel_run.py -i ./example_catalog_for_galsim.cat -o test -f r
+python ./galsim_no_pixel_run.py -i ./example_catalog_for_galsim.cat -o test -f z
 
